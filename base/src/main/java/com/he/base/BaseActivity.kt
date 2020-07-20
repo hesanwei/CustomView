e93586df77f50c49bc8b7f1438f94fe3d61ca36c
hesanwei <hesanwei.bj@acewill.cn>
2020-07-20 13:59:31 +0800

    自定义view 基础篇---MotionEvent 中的getX和getRawX的区别

--- base/src/main/java/com/he/base/BaseActivity.kt
+++ base/src/main/java/com/he/base/BaseActivity.kt
@@ -2,6 +2,8 @@ package com.he.base
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_base.*
 
@@ -11,8 +13,8 @@ import kotlinx.android.synthetic.main.activity_base.*
  * @author hesanwei created on 2020/7/20
  *
  */
-class BaseActivity: AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
+class BaseActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n", "ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
@@ -22,6 +24,12 @@ class BaseActivity: AppCompatActivity() {
 
         viewChild.post {
             tvInXY.text = "内部view  getLeft === ${viewChild.left}----getTop === ${viewChild.top}"
+        }
+
+        viewChild.setOnTouchListener { _, p1 ->
+            tvMotion.text =
+                "getX----${p1?.x} === getY----${p1?.y}\n getRawX----${p1?.rawX} === getRawY----${p1?.rawY}"
+            true
         }
     }
 }
